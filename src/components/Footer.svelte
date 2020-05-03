20f389221277b33b2fbf87b9cb12b426a8cbc62e
José Yáñez <jose.yanez@live.com>
2020-05-02 19:09:38 -0500

    add svelte app

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<script>
+    let year = new Date().getFullYear();
+    let footerText = `© ${year} Pugstagram From Platzi`;
+</script>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+    <div class="Footer-content">
+        <div class="Footer-copy">
+            <p>
+            <span>{footerText}</span>
+            </p>
+        </div>
+    </div>
+</div>
